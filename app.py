4169086ad3cf3a2a3034c53c413096b7f8a6e6ad
jack guerrero guerrero <jackguerreroguerrero@MacBook-Air-de-jack.local>
2026-04-08 11:06:30 -0600

    actualizacion app.py

--- app.py
+++ app.py
@@ -168,5 +168,8 @@ def mensaje(data):
         print("ERROR EN MENSAJE:", e)
 
 # ================= RUN =================
+
+import os
+
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
